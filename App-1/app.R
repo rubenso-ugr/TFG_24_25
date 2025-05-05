d0c644de2fee625847fd649b19715eeaf302ac1b
Ruben Soriano Vidal <Ruben.Soriano.Vidal@vub.be>
2025-05-05 20:14:00 +0200

    Incorporación de cambios en la metodología dentro de la aplicación web.

--- App-1/app.R
+++ App-1/app.R
@@ -11,8 +11,6 @@ library(RandomFields)
 
 mis.colores <- colorRampPalette(c("white", "blue", "lightgreen", "yellow", "red"))
 
-T<- c(1,1,1)
-
 rea_to_show <- 1 #Realización a mostrar
 
 
@@ -39,6 +37,7 @@ ui <- page_sidebar(
     navset_card_underline(
       nav_panel("Primera simulación", plotOutput("primera_simulacion"), downloadButton("downloadPlot1", "Descargar imagen PNG", disabled = TRUE)),
       nav_panel("Conjunto Excursión", plotOutput("conjunto_excursion"), downloadButton("downloadPlot2", "Descargar imagen PNG", disabled = TRUE)),
+      nav_panel("Mapa de varianzas",  plotOutput("mapa_varianzas"), downloadButton("downloadPlot3", "Descargar imagen PNG", disabled = TRUE)),
       nav_panel("Metodología aplicada",
                 plotOutput("metodología"),
                 selectInput("medida", "Medida de riesgo a usar", 
@@ -117,21 +116,17 @@ server <- function(input, output, session) {
     
     x <- 0:(input$ancho - 1)
     y <- 0:(input$alto - 1)
+    
     sim <- RFsimulate(model = modelo, x = x, y = y, T = c(1, 1, 1), n = input$realizaciones)
     
     sim_values <- matrix(unlist(sim@data), nrow = input$ancho * input$alto, ncol = input$realizaciones)
     
-    min_val_rea1 <- min(sim_values[, rea_to_show])
+
     min_val <- min(sim_values)
-    diferencia <- min_val - min_val_rea1
-    max_val_rea1 <- max(sim_values[, rea_to_show])
+    max_val <- max(sim_values)
+    max_val_rea <- max(sim_values[, rea_to_show])
     
-    
-    sim_values_pos_rea1 <- sim_values[, rea_to_show] - min_val_rea1
-    
-    sim_values_pos <- sim_values - min_val  # ahora el mínimo de la primera realización es 0
-    
-    realization_matrix <- matrix(sim_values_pos[, rea_to_show], nrow = input$ancho, ncol = input$alto)
+    realization_matrix <- matrix(sim_values[, rea_to_show], nrow = input$ancho, ncol = input$alto)
     
     
     output$primera_simulacion <- renderPlot({
@@ -143,23 +138,19 @@ server <- function(input, output, session) {
                      frame.plot = 0,
                      main = paste("Realización", rea_to_show),
                      xlim =  c(0, input$ancho),
-                     ylim =  c(0, input$alto))
+                     ylim =  c(0, input$alto),
+                     zlim = c(min_val, max_val_rea))
     })
     
     
     
-    #Guardamos el valor máximo global y de la primera realización.
-    #max_val <- max(sim_values_pos)
-    max_val_rea1 <- max(sim_values_pos_rea1)
-    
-    
     #Estimamos el umbral de riesgo (percentil 1 - alpha)
-    all_vals <- as.vector(sim_values_pos)
+    all_vals <- as.vector(sim_values)
     threshold <- quantile(all_vals, probs = 1 - input$percentil)
     
-    #Obtenemos el conjunto de excursión (asignamos 0 a valores por debajo del umbral en TODAS las realizaciones)
-    excursion_values <- sim_values_pos
-    excursion_values[sim_values_pos < threshold] <- 0
+    #Obtenemos el conjunto de excursión (asignamos min_val a valores por debajo del umbral en TODAS las realizaciones)
+    excursion_values <- sim_values
+    excursion_values[sim_values < threshold] <- min_val
     
     #Tomamos el conjunto de excursión de la realización escogida
     excursion_matrix <- matrix(excursion_values[, rea_to_show], nrow = input$ancho, ncol = input$alto)
@@ -173,8 +164,31 @@ server <- function(input, output, session) {
                      main = paste("Conjunto de Excursión (Realización", rea_to_show, ")"),
                      xlim =  c(0, input$ancho),
                      ylim =  c(0, input$alto),
-                     zlim = c(min_val_rea1 - min_val, max_val_rea1 - diferencia))
+                     zlim = c(min_val, max_val_rea))
     })
+    
+    
+    #Mapa varianzas --------------------------------------------------------------------
+    
+    # Calculamos la varianza por fila (cada píxel a través de las n simulaciones)
+    varianzas_vector <- apply(sim_values, 1, var)
+    
+    # Lo reestructuramos a matriz 200x200
+    mapa_varianzas <- matrix(varianzas_vector, nrow = input$alto, ncol = input$ancho)
+    
+    #Dibujamos el mapa de varianas de las simulaciones realizadas
+    output$mapa_varianzas <- renderPlot({
+      filled.contour(x, y, mapa_varianzas,
+                     color.palette = mis.colores,
+                     asp = 1,
+                     axes = TRUE,
+                     frame.plot = 0,
+                     main = paste("Mapa de varianzas"),
+                     xlim = c(0, input$ancho),
+                     ylim = c(0, input$alto))
+    })
+    
+    
     
     
     #Metodologia -----------------------------------------------------------------------
@@ -187,10 +201,10 @@ server <- function(input, output, session) {
     alpha2 <- 0.05  #Segundo percentil a usar
     
     # Creamos una copia exacta de sim_values_pos para cada medida y metodologia a usar
-    refined_values_hist <- excursion_values  # VaR histórico
-    refined_values_param <- excursion_values # VaR paramétrico
-    refined_values_mc <- excursion_values    # VaR Monte Carlo
-    refined_values_es <- excursion_values    # Expected Shortfall
+    refined_values_hist <- matrix(NA, nrow = input$ancho, ncol = input$alto)  # VaR histórico
+    refined_values_param <- matrix(NA, nrow = input$ancho, ncol = input$alto) # VaR paramétrico
+    refined_values_mc <- matrix(NA, nrow = input$ancho, ncol = input$alto)    # VaR Monte Carlo
+    refined_values_es <- matrix(NA, nrow = input$ancho, ncol = input$alto)    # Expected Shortfall
     
     # Recorremos la malla con ventanas deslizantes
     for (i in seq(1, input$ancho, by = step)) {
@@ -221,61 +235,52 @@ server <- function(input, output, session) {
           
           # VaR Histórico
           var_hist <- quantile(filtered_data, probs = 1 - alpha2) 
-          #refined_values_hist[linear_indices, ][mask_above_global] <- var_hist
           
           # Aplicamos el umbral obtenido a los valores de dentro de la ventana de todas las realizaciones que superaron el primer umbral 
-          tmp <- refined_values_hist[linear_indices, , drop = FALSE]
-          tmp[mask_above_global] <- var_hist
-          refined_values_hist[linear_indices, ] <- tmp
+          refined_values_hist[linear_indices] <- var_hist
           
           # VaR Paramétrico
           mu <- mean(filtered_data)     # media
           sigma <- sd(filtered_data)    # desv. tipica
           z_alpha <- qnorm(1 - alpha2)  # cuantil de distrib. normal estandar a nivel de confianza 1- alpha2
           var_param <- mu + sigma * z_alpha
-          #refined_values_param[linear_indices, ][mask_above_global] <- var_param
-          
           
           # Aplicamos el umbral obtenido a los valores de dentro de la ventana de todas las realizaciones que superaron el primer umbral 
-          tmp_param <- refined_values_param[linear_indices, , drop = FALSE]
-          tmp_param[mask_above_global] <- var_param
-          refined_values_param[linear_indices, ] <- tmp_param
+          
+          refined_values_param[linear_indices] <- var_param
           
           
           
           # VaR Monte Carlo (simulación normal simple con misma media y sigma)
           sim_mc <- rnorm(10000, mean = mu, sd = sigma)   # conjunto de simulaciones
           var_mc <- quantile(sim_mc, probs = 1 - alpha2)
-          #refined_values_mc[linear_indices, ][mask_above_global] <- var_mc
           
           # Aplicamos el umbral obtenido a los valores de dentro de la ventana de todas las realizaciones que superaron el primer umbral 
-          tmp_mc <- refined_values_mc[linear_indices, , drop = FALSE]
-          tmp_mc[mask_above_global] <- var_mc
-          refined_values_mc[linear_indices, ] <- tmp_mc
+          refined_values_mc[linear_indices] <- var_mc
           
           
           # Expected Shortfall histórico
           es_val <- mean(filtered_data[filtered_data >= var_hist])
-          #refined_values_es[linear_indices, ][mask_above_global] <- es_val
           
           # Aplicamos el umbral obtenido a los valores de dentro de la ventana de todas las realizaciones que superaron el primer umbral 
-          tmp_es <- refined_values_es[linear_indices, , drop = FALSE]
-          tmp_es[mask_above_global] <- es_val
-          refined_values_es[linear_indices, ] <- tmp_es
-          
+          refined_values_es[linear_indices] <- es_val
           
         }
       }
     }
     
-    # Creamos matrices 2D de cada resultado para una realización
-    matrix_VaR_hist <- matrix(refined_values_hist[, rea_to_show], nrow = input$ancho, ncol = input$alto)
-    matrix_VaR_param <- matrix(refined_values_param[, rea_to_show], nrow = input$ancho, ncol = input$alto)
-    matrix_VaR_mc <- matrix(refined_values_mc[, rea_to_show], nrow = input$ancho, ncol = input$alto)
-    matrix_ES <- matrix(refined_values_es[, rea_to_show], nrow = input$ancho, ncol = input$alto)
+   
     
-    #Obtenemos el maximo de entre todas las simulaciones para que la comparación de colores sea correcta
+    #Obtenemos el maximo y el mínimo entre todas las simulaciones para que la comparación de colores sea correcta
     max_z <- max(
+      refined_values_hist,
+      refined_values_param,
+      refined_values_mc,
+      refined_values_es,
+      na.rm = TRUE  # por si acaso hay NA
+    )
+    
+    min_z <- min(
       refined_values_hist,
       refined_values_param,
       refined_values_mc,
@@ -289,42 +294,41 @@ server <- function(input, output, session) {
     
     output$metodología <- renderPlot({
       switch(input$medida,
-             "VaR Histórico" = filled.contour(x, y, matrix_VaR_hist,
+             "VaR Histórico" = filled.contour(x, y, refined_values_hist,
                                               color.palette = mis.colores,
                                               asp = 1,
                                               axes = TRUE,
                                               frame.plot = 0,
-                                              main = paste("VaR Histórico 95% - Realización", rea_to_show),
+                                              main = sprintf("VaR Histórico %.0f%%", (1 - alpha2) * 100),
                                               xlim = c(0, input$ancho),
-                                              ylim = c(0, input$alto),
-                                              zlim = c(0, max_z)),
-             "VaR Paramétrico" = filled.contour(x, y, matrix_VaR_param,
+                                              ylim = c(0, input$alto)),
+             
+             "VaR Paramétrico" = filled.contour(x, y, refined_values_param,
                                                 color.palette = mis.colores,
                                                 asp = 1,
                                                 axes = TRUE,
                                                 frame.plot = 0,
-                                                main = paste("VaR Paramétrico 95% - Realización", rea_to_show),
+                                                main = sprintf("VaR Paramétrico %.0f%%", (1 - alpha2) * 100),
                                                 xlim = c(0, input$ancho),
-                                                ylim = c(0, input$alto),
-                                                zlim = c(0, max_z)),
-             "VaR Montecarlo" =  filled.contour(x, y, matrix_VaR_mc,
+                                                ylim = c(0, input$alto)),
+             
+             "VaR Montecarlo" =  filled.contour(x, y, refined_values_mc,
                                                 color.palette = mis.colores,
                                                 asp = 1,
                                                 axes = TRUE,
                                                 frame.plot = 0,
-                                                main = paste("VaR Monte Carlo 95% - Realización", rea_to_show),
+                                                main = sprintf("VaR Monte Carlo %.0f%%", (1 - alpha2) * 100),
                                                 xlim = c(0, input$ancho),
-                                                ylim = c(0, input$alto),
-                                                zlim = c(0, max_z)),
-             "ES" = filled.contour(x, y, matrix_ES,
+                                                ylim = c(0, input$alto)),
+             
+             "ES" = filled.contour(x, y, refined_values_es,
                                    color.palette = mis.colores,
                                    asp = 1,
                                    axes = TRUE,
                                    frame.plot = 0,
-                                   main = paste("Expected Shortfall 95% - Realización", rea_to_show),
+                                   main = sprintf("Expected Shortfall %.0f%%", (1 - alpha2) * 100),
                                    xlim = c(0, input$ancho),
-                                   ylim = c(0, input$alto),
-                                   zlim = c(0, max_z)))
+                                   ylim = c(0, input$alto)))
     })
     
 
@@ -336,9 +340,8 @@ server <- function(input, output, session) {
     
     output$mensaje_resumen <- renderPrint({
       cat("Min. global:", min_val, "\n")
-      cat("Min. realización mostrada:", min_val_rea1, "\n")
-      cat("Diferencia:", diferencia, "\n")
-      cat("Max. realización mostrada:", max_val_rea1, "\n")
+      cat("Max. global:", max_val, "\n")
+      cat("Max. realizacion mostrada:", max_val_rea, "\n")
       cat("Umbral: ", threshold, "\n")
     })
     
@@ -346,7 +349,7 @@ server <- function(input, output, session) {
     
     output$downloadPlot1 <- downloadHandler(
       filename = function() {
-        paste("primera_simulacion", Sys.Date(), ".png", sep = "")
+        paste("simulacion", Sys.Date(), ".png", sep = "")
       },
       content = function(file) {
         png(file, width = 800, height = 800)
@@ -364,7 +367,7 @@ server <- function(input, output, session) {
     
     output$downloadPlot2 <- downloadHandler(
       filename = function() {
-        paste("primera_simulacion", Sys.Date(), ".png", sep = "")
+        paste("simulacion_excursion", Sys.Date(), ".png", sep = "")
       },
       content = function(file) {
         png(file, width = 800, height = 800)
@@ -377,6 +380,24 @@ server <- function(input, output, session) {
                        xlim =  c(0, input$ancho),
                        ylim =  c(0, input$alto),
                        zlim = c(min_val_rea1 - min_val, max_val_rea1 - diferencia))
+        dev.off()
+      }
+    )
+    
+    output$downloadPlot3 <- downloadHandler(
+      filename = function() {
+        paste("mapa_varianzas", Sys.Date(), ".png", sep = "")
+      },
+      content = function(file) {
+        png(file, width = 800, height = 800)
+        filled.contour(x, y, excursion_matrix,
+                       color.palette = mis.colores,
+                       asp = 1,
+                       axes = TRUE,
+                       frame.plot = 0,
+                       main = paste("Mapa de Varianzas"),
+                       xlim =  c(0, input$ancho),
+                       ylim =  c(0, input$alto))
         dev.off()
       }
     )
@@ -396,6 +417,12 @@ server <- function(input, output, session) {
   observeEvent(input$button, {
     shinyjs::enable("downloadPlot2")
   })
+  
+  # Habilitamos el botón de descarga una vez haya un gráfico
+  observeEvent(input$button, {
+    shinyjs::enable("downloadPlot3")
+  })
+  
 }
 
  

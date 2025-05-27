064ab8e2c3ef396a2235590f440d036268edd2a7
Ruben Soriano Vidal <Ruben.Soriano.Vidal@vub.be>
2025-05-27 18:56:30 +0200

    Reorganización de código y comentarios sobre el mismo.

--- App-1/server.R
+++ App-1/server.R
@@ -5,24 +5,25 @@ library(RandomFieldsUtils)
 library(RandomFields)
 
 source("procesamiento_simulaciones.R")
-source("funciones_auxiliares.R")
+
+# Opciones globales
+options(shiny.useragg = FALSE)
+set.seed(123)
 
 # Definición del server ----------------------------------------------------------------------------
 
 server <- function(input, output, session) {
   
-  # 1. Crear la variable reactiva para almacenar el modelo con el que se ejecutó la simulación
+  # Creación de una variable reactiva para almacenar el modelo con el que se ejecutó la simulación
   modelo_ejecutado <- reactiveVal(NULL)
   
-  ### Manejo de Modales y Formularios Dinámicos ###
-  
-  # Mostrar el modal para modo 'Simulación Condicionada'
-  observeEvent(input$button, {
+  # Tratamiento de la ejecución cuando se pulsa el botón de simulación
+  observeEvent(input$boton_simular, {
     
-    # 2. Al pulsar el botón, guardar el modelo actual como el ejecutado
+    # Al pulsar el botón, se guarda el modelo actual como el ejecutado
     modelo_ejecutado(input$modelo)
     
-    # Validación de parámetros
+    # Validación de parámetros comunes a ambos tipos de simulación
     if (!validar_parametro_ab_cer("alpha", input$alpha, 0, 2)) return(NULL)
     if (!validar_parametro_ab_ab("beta", input$beta, 0)) return(NULL)
     if (!validar_parametro_ab_ab("var", input$var, 0)) return(NULL)
@@ -36,7 +37,7 @@ server <- function(input, output, session) {
     x <- 0:(input$ancho)
     y <- 0:(input$alto)
     
-    
+    # Lógica de simulación condicionada #
     if (input$modo == "Simulación Condicionada") {
       
       #Dependiendo del tipo de simulación se establecen unos máximos sobre los atributos ancho, alto y realizaciones
@@ -53,21 +54,20 @@ server <- function(input, output, session) {
       #Se muestra una ventana para permitir al usuario introducir valores condicionados, ya sea manualmente o mediante un archivo .csv
       showModal(modalDialog(
         title = "Valores condicionales",
-        fileInput("file_input", "Sube un archivo CSV", accept =".csv"),
+        fileInput("file_input", "Sube un archivo .csv", accept =".csv"),
         tagList(
           numericInput("n_filas_modal", "Número de valores:", value = 1, min = 1),
           uiOutput("formulario_dinamico")
         ),
         footer = tagList(
           modalButton("Cerrar"),
-          actionButton("calcular_formulario", "Simular formulario"),
+          actionButton("calcular_formulario", "Simular mediante formulario"),
           actionButton("procesar_archivo", "Procesar archivo")
         ),
         size = "l", easyClose = TRUE
       ))
-    } else if (input$modo == "Simulación") {      ### Lógica de simulación no condicionada###
-      #Se realiza la simulación no condicionada y se lanzan las diferentes funciones de la metodología conlos datos simulados en función del modelo de simulación escogido
-      
+    } else if (input$modo == "Simulación") {      # Lógica de simulación no condicionada #
+
       #Se establecen unos máximos sobre los atributos ancho, alto y realizaciones para simulación no condicionada
       
       if (!validar_parametro_cer_ab("ancho (píxeles)", input$ancho, 3)) return(NULL)
@@ -90,19 +90,17 @@ server <- function(input, output, session) {
           psi = RMstable(alpha = input$alpha, var = input$var, scale = input$scale),
           delta = 2
         )
-        
         sim <- RFsimulate(model = modelo, x = x, y = y, T = 1:4, n = input$realizaciones)
         sim_values <- matrix(unlist(sim@data), nrow = (input$ancho+1) * (input$alto+1) *4, ncol = input$realizaciones)
         #write.csv(as.data.frame(sim_values), "gneiting.csv", row.names = FALSE)
         threshold_temporal <- modulo_simulacion_temporal(sim_values, x, y, input, output)
-        
         modulo_metodologia_temporal(sim_values, x, y, input, output, threshold_temporal)
         
       }
     }
   })
   
-  # Generación dinámica del formulario dentro del modal
+  # Generación dinámica del formulario dentro del modal para simulación condicionada mediante formulario
   observe({
     req(input$n_filas_modal)
     output$formulario_dinamico <- renderUI({
@@ -118,8 +116,9 @@ server <- function(input, output, session) {
     })
   })
   
-  # Procesamiento de los datos del formulario al hacer clic en 'Simular formulario'
+  # Procesamiento de los datos del formulario al hacer clic en 'Simular mediante formulario'
   observeEvent(input$calcular_formulario, {
+    
     req(input$n_filas_modal)
     n <- input$n_filas_modal
     data <- data.frame(
@@ -128,8 +127,8 @@ server <- function(input, output, session) {
       coords.x2 = sapply(1:n, function(i) input[[paste0("fila_", i, "_val1")]]),
       coords.x3 = rep(1, n)
     )
-    
     procesar_datos(data, input, output)
+    
   })
   
   
@@ -137,12 +136,11 @@ server <- function(input, output, session) {
   observeEvent(input$procesar_archivo, {
     
     req(input$file_input)
-    
-    # Leer el archivo CSVy validar parasimulación incondicionada
+    # Leer el archivo CSV y validar parasimulación incondicionada
     data <- leer_y_validar_archivo(input$file_input, añadir_z = TRUE)
     if (is.null(data)) return(NULL)
-    
     procesar_datos(data, input, output)
+    
   })
   
   
@@ -182,59 +180,48 @@ server <- function(input, output, session) {
     }
   })
   
-  
-  
-  
-  # 3. Crear una salida reactiva para usar en conditionalPanel
-  output$mostrar_temporal <- reactive({
-    modelo_ejecutado() == "Gneiting"
-  })
-  
-  # 4. Permitir que Shiny use esa salida en el UI dinámico
-  outputOptions(output, "mostrar_temporal", suspendWhenHidden = FALSE)
-  
-  
-  
-  observeEvent(input$button_2, {
+  # Tratamiento de opción de adjuntar datos ya simulados mediante un archivo
+  observeEvent(input$boton_adjuntar, {
     modelo_ejecutado(input$modelo)
     showModal(
       modalDialog(
         title = "Adjuntar Datos",
-        fileInput("archivo", "Selecciona un archivo:"),
+        fileInput("archivo", "Selecciona un archivo .csv:"),
         
         # Input adicional solo en modo 'Simulación Condicionada'
         conditionalPanel(
           condition = "input.modo == 'Simulación Condicionada'",
-          fileInput("archivo_datos_condicionados", "Selecciona un archivo con datos condicionados:")
+          fileInput("archivo_datos_condicionados", "Selecciona un archivo .csv con los datos condicionados usados:")
         ),
-        
-        # Botón para modo 'Simulación'
-        conditionalPanel(
-          condition = "input.modo == 'Simulación'",
-          actionButton("confirmar_adjunto_simulacion", "Cargar (Simulación)")
+        footer = tagList(
+          modalButton("Cerrar"),
+          # Botón para modo 'Simulación'
+          conditionalPanel(
+            condition = "input.modo == 'Simulación'",
+            actionButton("confirmar_adjunto_simulacion", "Cargar datos")
+          ),
+          
+          # Botón para modo 'Simulación Condicionada'
+          conditionalPanel(
+            condition = "input.modo == 'Simulación Condicionada'",
+            actionButton("confirmar_adjunto_condicionada", "Cargar datos")
+          )
+          
         ),
-        
-        # Botón para modo 'Simulación Condicionada'
-        conditionalPanel(
-          condition = "input.modo == 'Simulación Condicionada'",
-          actionButton("confirmar_adjunto_condicionada", "Cargar (Condicionada)")
-        ),
-        
-        easyClose = TRUE,
-        footer = NULL
-      )
+        size = "l", easyClose = TRUE
+        )
     )
   })
   
+  #Tratamiento de adjunto de valores simulados no condicionados
   observeEvent(input$confirmar_adjunto_simulacion, {
     
     req(input$archivo)
     
-    # Leer el archivo tal cual, sin cabecera
+    # Se lee el archivo omitiendo la cabecera
     sim_values <- as.matrix(readr::read_csv(input$archivo$datapath, col_names = TRUE, show_col_types = FALSE))
     
     # Validación de dimensiones 
-    
     if (!validar_dimensiones_matriz(sim_values, input$modelo, input$ancho, input$alto, input$realizaciones)) {
       return(NULL)
     }
@@ -270,7 +257,6 @@ server <- function(input, output, session) {
     
     
     # Validación de dimensiones 
-    
     if (!validar_dimensiones_matriz(sim_values, input$modelo, input$ancho, input$alto, input$realizaciones)) {
       return(NULL)
     }
@@ -289,5 +275,14 @@ server <- function(input, output, session) {
     
     removeModal()
   })
+  
+  
+  # Se crea una salida reactiva para usar en conditionalPanel
+  output$mostrar_temporal <- reactive({
+    modelo_ejecutado() == "Gneiting"
+  })
+  
+  # Permitir que Shiny use esa salida en el UI dinámico
+  outputOptions(output, "mostrar_temporal", suspendWhenHidden = FALSE)
   
 }
